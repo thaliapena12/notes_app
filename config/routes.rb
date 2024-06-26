ce24ea6d8b382bda21b0a0e1da2d807f623952ed
Thalia Pena <55005669+thaliapena12@users.noreply.github.com>
2024-06-26 19:18:50 -0400

    add routes for notes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  Rails.application.routes.draw do
+    resources :notes
+    root "notes#index"
+  end
 end
